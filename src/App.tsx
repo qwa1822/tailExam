89131404de177b8592bb6aa5c1f623ea628fd9ad
qwa1822 <asdzxc1822@naver.com>
2024-03-11 15:08:25 +0900

    'loginForm'

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 import Card from "./components/Card";
 import Image from "./components/Image";
+import LoginModal from "./components/LoginModal";
 import Modal from "./components/Modal";
 import Price from "./components/Price";
 
 function App() {
   return (
     <>
-      <Image />
+      <LoginModal />
     </>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ["Mulish", "sans-serif"],
+        mono: ["Rokkitt", "monospace"],
+      },
+    },
   },
   plugins: [],
 };
